a313def54eb36e495e5346d719d7b485270cafe3
Vaibhav Bhardwaj <vaibhavbhardwaj3010@gmail.com>
2025-09-03 19:24:59 +0530

    finished the programming

--- counter program/index.js
+++ counter program/index.js
@@ -1,1 +1,21 @@
-// js program and logic building
+let count = 0;
+
+function increase() {
+  count += 1;
+  document.getElementById("H2").textContent = count;
+}
+
+function decrease() {
+  count -= 1;
+  document.getElementById("H2").textContent = count;
+}
+
+function reset() {
+  count = 0; 
+  document.getElementById("H2").textContent = count;
+}
+
+document.getElementById("H2").textContent = count;
+document.getElementById("b1").onclick = decrease;
+document.getElementById("b2").onclick = reset;
+document.getElementById("b3").onclick = increase;
